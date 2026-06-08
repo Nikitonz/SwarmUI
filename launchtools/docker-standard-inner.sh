849c53b57f975168bd0a2794f8339f471009f75f
Nikitonz <nikitoniy2468@gmail.com>
2026-06-08 11:27:27 +0300

    docker: keep configured home for runtime user

--- launchtools/docker-standard-inner.sh
+++ launchtools/docker-standard-inner.sh
@@ -24,8 +24,11 @@ then
     exit
 fi
 
-# Add a fake home path, because docker defaults it to '/'
-HOME=/SwarmUI/dlbackend/linuxhome
+# Add a fake home path only when docker does not provide a usable one.
+if [ -z "$HOME" ] || [ "$HOME" == "/" ]
+then
+    export HOME=/SwarmUI/dlbackend/linuxhome
+fi
 
 # Launch as normal, just ensure launch mode is off and host is global (to expose it out of the container)
 bash /SwarmUI/launch-linux.sh "$@" --launch_mode none --host 0.0.0.0
